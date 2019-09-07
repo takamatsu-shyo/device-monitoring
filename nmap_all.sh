a872004f0aef7db9671c52a66b72b1e59cea6685
syano <takamatsu.shyo@gmail.com>
2019-09-07 13:46:13 +0300

    Clean up codes

--- nmap_all.sh
+++ nmap_all.sh
@@ -4,9 +4,11 @@
 while IFS= read -r line
 do
     dir=$(echo "$line" | sed 's/\./-/g' |  sed 's/^/\/tmp\/log\/nmapA\//g')
+    date_str=$(date -Iminute)
+    logfile=""$date_str"_log"
     echo "$dir"
     echo "$dir" | xargs mkdir -p
     cd "$dir"
-    echo "$line" | sudo xargs nmap -A > log 
+    echo "$line" | sudo xargs nmap -A > $logfile 
     cd -
 done < /tmp/uniq_ip
