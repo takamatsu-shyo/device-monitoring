89ae195eb9cf525d484b15fe3bdb961e07905f88
syano <takamatsu.shyo@gmail.com>
2019-09-15 15:50:31 +0300

    Debug uniq_ip

--- get_list.sh
+++ get_list.sh
@@ -2,17 +2,14 @@
 
 # set cron job to this script
 
-#TODO: web interface 
-#TODO: log rotate
-#TODO: mail integration
-
 sudo nmap -sn 192.168.1.1-255 > /tmp/nmap_list
 
-#-----------------------------------------
-# nmap -A for all device with external script
-# nmap_all.sh
-cat /tmp/all_list | 
+cat /tmp/nmap_list | 
 grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | 
 sort | 
-uniq > /tmp/uniq_ip
+uniq > /tmp/uniq_ip_now
 
+cp /tmp/uniq_ip /tmp/uniq_ip_before
+cat /tmp/uniq_ip_now /tmp/uniq_ip_before |
+sort | 
+uniq > /tmp/uniq_ip
