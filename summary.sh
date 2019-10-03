a8f41abe71a5b4503dbf1358eee5331d7f9c4619
tkmsy <takamatsu.shyo@gmail.com>
2019-10-03 22:36:25 +0300

    summary.sh improved

--- summary.sh
+++ summary.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sudo nmap 192.168.1.1-255 -sn > /tmp/summary.txt
+cp $HOME/log/summary.txt $HOME/log/summary_last
 
-cat /tmp/summary.txt  | 
+#sudo nmap 192.168.1.1-255 -sn > /tmp/summary.txt
+cp  $HOME/log/nmap_list /tmp/summary
+
+cat /tmp/summary |
 grep -v 'Nmap done' | 
 grep -v 'Starting' | 
 grep -v 'Host is up' | 
@@ -11,16 +14,20 @@ sed  's/Nmap scan report for //g' |
 sed 's/MAC address: //g'  > /tmp/summary_.txt
 
 cat /tmp/summary_.txt | 
-awk 'match($1,/[0-9].*[0-9]/){print $0}' > /tmp/summary_nohostname
+awk 'match($1,/[0-9].*[0-9]/){print $0}' |
+sed 's/MAC Address: //g' > /tmp/summary_nohostname
 
 cat /tmp/summary_.txt | 
 awk 'match($2,/([0-9].*[0-9])/){print $2" " $1" "$5" "$6}' | 
 sed 's/^(//g' | 
 sed 's/) / /g' > /tmp/summary_hostname 
 
-cat /tmp/summary_hostname /tmp/summary_nohostname > ~/log/summary.txt
+cat /tmp/summary_hostname /tmp/summary_nohostname |
+sort -t . -k 3,3n -k 4,4n > $HOME/log/summary_now
+
+cat $HOME/log/summary_now $HOME/log/summary_last |
+sort -t . -k 3,3n -k 4,4n |
+uniq > $HOME/log/summary.txt
+
 
 #TODO: hostname, if MAC manufacturer contain space then lose after first space
-#TODO: nohostname, MAC Address is remained
-#TODO: -sn result can be obtained from get_nmaplist.sh result
-#      think about how to get largest log since sometimes failed to -sn
